c09a190c0d44b7199c91ad92403fc65df8b5a6e3
SAUMYA SINHA <saumyasinha269@gmail.com>
2024-06-17 12:59:18 +0530

    update

---
+++ 03_variables.sh
@@ -0,0 +1,38 @@
+
+
+
+
+#!/bin/bash
+#we are going to learn using variables here
+
+a=10
+name="saumya sinha"
+sem=6
+
+echo "my name is $name"
+ 
+echo "value of a is $a"
+echo "my sem is $sem"
+
+#changing value of variables
+
+a=15
+
+
+
+echo "value of a is $a"
+
+#variable to store the output of a command
+
+HOSTNAME = $(hostname)
+echo "name od this machine is $HOSTNAME"
+
+
+ 
+
+
+
+
+
+ 
+
---
+++ 04_constantvariables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+readonly name="saumya_sinha"
+
+echo "now you can't change my name. It is $name only"
+
+
+
+
+
+
+
+
+
+
+
